3aeec63c52bf16f3bca490a952eb9e19032956f8
WilliamNorboe <98242626+WilliamNorboe@users.noreply.github.com>
2024-09-17 15:23:52 -0700

    linked css file

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,6 +12,6 @@ class Post extends Model
     protected $fillable = ['title', 'body', 'user_id'];
 
     public function user(){
-        return $this->belongsTo(User::class, 'user_id');
+           return $this->belongsTo(User::class, 'user_id');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 </head>
 <body>
     @auth
@@ -49,7 +50,7 @@
             <input name = "name" type = "text" placeholder = "name">
             <input name = "email" type = "text" placeholder = "email">
             <input name = "password" type = "password" placeholder = "password">
-            <button>Register</button>
+            <button>Register</button> 
         </form>
     </div>
 
@@ -59,13 +60,12 @@
             @csrf
             <input name = "loginname" type = "text" placeholder = "name">
             <input name = "loginpassword" type = "password" placeholder = "password">
-            <button>Login in</button>
+            <button>Login</button>
         </form>
     </div>
 
 
 
     @endauth
-
 </body>
 </html>
